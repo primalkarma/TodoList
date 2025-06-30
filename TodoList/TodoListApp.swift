54e4153a20858543e58db6ee883c32a3b4ad7d15
primalkarma <primalkarma@gmail.com>
2025-06-30 05:49:23 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Parimal Devi on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
